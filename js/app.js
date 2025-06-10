5e420c2a81b6b1a9a7adf3a962469034f29451e1
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2025-06-10 22:32:18 +0700

    M download scale

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 function downloadApplication() {
-  const formElement = document.querySelector("#application-form");
-  html2canvas(formElement).then(canvas => {
+  const form = document.getElementById("application-form");
+
+  const originalWidth = form.style.width;
+  const originalTransform = form.style.transform;
+  const originalScale = form.style.scale;
+
+  form.style.width = "850px";
+  form.style.transform = "scale(1)";
+  form.style.transformOrigin = "top left";
+
+  html2canvas(form, {
+    scale: 2, 
+    width: 850,
+    windowWidth: 850,
+  }).then(canvas => {
     const link = document.createElement("a");
     link.download = "job-application.png";
     link.href = canvas.toDataURL("image/png");
     link.click();
+
+    form.style.width = originalWidth;
+    form.style.transform = originalTransform;
   });
 }
